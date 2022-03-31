bd4cf365f7e9a1ca5a352c31a3bfbc4d2ab818f6
Maxim Abramov <abramov.ms@phystech.edu>
2022-03-31 12:22:21 +0300

    Add context retrival

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ std::vector<size_t> PrefixFunction(const std::string &subject) {
 
 size_t PrefixFunctionNext(
     const std::string &pattern,
-    const std::vector<std::size_t>& pattern_prefix_fn,
+    const std::vector<std::size_t> &pattern_prefix_fn,
     char next_text_char,
     std::size_t prev_suprefix_size) {
   std::size_t suprefix_size = prev_suprefix_size;
@@ -46,6 +46,34 @@ size_t PrefixFunctionNext(
   return suprefix_size;
 }
 
+std::string GetContext(std::ifstream &file, std::size_t content_size,
+                       std::streamoff context_size) {
+  auto current_pos = file.tellg();
+  auto context_start = current_pos - std::streamoff(content_size) - context_size;
+
+  file.seekg(context_start);
+
+  std::string context;
+  for (size_t i = 0; i < content_size + 2 * context_size; ++i) {
+    auto text_char = file.get();
+    if (text_char == EOF) {
+      break;
+    }
+
+    if (text_char == '\n') {
+      text_char = ' ';
+    }
+
+    context.push_back(static_cast<char>(text_char));
+  }
+
+  file.seekg(current_pos);
+
+  return context;
+}
+
+const int kContextSize = 20;
+
 int main() {
   std::string filename;
   std::cout << "Path to text file: ";
@@ -53,8 +81,10 @@ int main() {
 
   std::ifstream file(filename);
 
-  size_t occurrences = 0;
+  auto begin = file.tellg();
+
   while (true) {
+    size_t occurrences = 0;
     std::string query;
     std::cout << "> ";
     std::cin >> query;
@@ -71,12 +101,17 @@ int main() {
           PrefixFunctionNext(query, query_prefix_fn,
                              static_cast<char>(next_text_char), suprefix_size);
       if (suprefix_size == query.size()) {
-        std::cout << "Found occurrence!" << std::endl;
+        auto context = GetContext(file, query.size(), kContextSize);
+        std::cout << "..." << context << "..." << std::endl;
         ++occurrences;
       }
     }
+
+    file.clear();
+    file.seekg(begin);
+
+    std::cout << "Found " << occurrences << " occurrences." << std::endl;
   }
 
-  std::cout << "Found " << occurrences << " occurrences." << std::endl;
   std::cout << "Search done. Have a good day!" << std::endl;
 }
